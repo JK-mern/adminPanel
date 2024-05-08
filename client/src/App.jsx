8166e2b099fce36b1a45ba7af8d517aeee23ee52
JK-mern <Jkmerndev@gmail.com>
2024-05-09 00:22:44 +0530

    created a route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 function App() {
-  return <></>;
+  return <>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/login" element />
+  </Routes>
+  </BrowserRouter>
+  </>;
 }
 
 export default App;
